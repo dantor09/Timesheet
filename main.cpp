e2cdd08bdd32ef0a0c163c0c7e9785c5909bdfc7
Daniel Torres <D_Torres39@outlook.com>
2021-06-20 20:54:58 -0700

    Fixed bug where time accumulated would not show if amount of nodes were odd rather than even

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ class LinkedList
 private:
     Family *head;
     Family *copy;
-
+    int number_of_nodes;
     Family *create()
     {
         Family *newnode = new Family;
@@ -29,31 +29,29 @@ public:
     {
         head = NULL;
         copy = head;
-    
-        
+        int number_of_nodes = 0;
     }
     int accumulate_hours()
     {
-        Family * copy = head;
+        Family *copy = head;
         int answer;
-        int accumulation = 0;
-        while(copy != NULL)
+        int accumulation = copy->hours;
+        while (copy->next != NULL)
         {
-            accumulation = accumulation+ copy->hours + copy->next->hours;
-            copy = copy->next->next;
+            accumulation = accumulation + copy->next->hours;
+            copy=copy->next;
         }
         answer = accumulation;
-        return answer ;
+        return answer;
     }
     void add(int n) //The add function adds a sequence of FAMILY nodes to the end, which each have an array size 20
     {
-        
 
         Family *newnode = create();
 
         if (head != NULL)
         {
-            
+
             while (copy->next != NULL)
             {
                 copy = copy->next;
@@ -61,57 +59,55 @@ public:
             copy->next = newnode;
             copy->next->hours = n;
             newnode->next = NULL;
-           
-            
+            number_of_nodes++;
+
             copy = head;
-           
         }
         if (head == NULL)
         {
-            
+
             head = newnode;
             head->hours = n;
             head->next = NULL;
-            
-            
+            number_of_nodes++;
+
             copy = head;
         }
     }
     Family *gethead() { return head; }
     Family *getcopy() { return copy; }
-    friend void show(LinkedList & Object ); //This show function goes through each FAMILY node and shows family members;
-    
+    friend void show(LinkedList &Object); //This show function goes through each FAMILY node and shows family members;
 };
-void show(LinkedList & Object ) 
-    {
-        Family * copy = Object.head;
-        cout << endl;
+void show(LinkedList &Object)
+{
+    Family *copy = Object.head;
+    cout << endl;
 
-        while (copy != NULL)
-        {
-            cout << copy->hours <<endl;
-            copy = copy->next;
-        }
-       cout<<"Hours accumulated: "<< Object.accumulate_hours()<<endl;
+    while (copy != NULL)
+    {
+        cout << copy->hours << endl;
+        copy = copy->next;
     }
+    cout << "Hours accumulated: " << Object.accumulate_hours() << endl;
+}
 
 int main()
 {
-    
 
     LinkedList one;
     LinkedList two;
-    one.add(9);
-    one.add(1);
-    one.add(7);
-    one.add(1);
-    two.add(8);
-    two.add(11);
-    two.add(73);
-    two.add(12);
+
+    int hours;
+    string array[7] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+    for (int i = 0; i < 7; i++)
+    {
+        cout << "How many hours did you work on: " << array[i];
+        cin >> hours;
+        one.add(hours);
+    }
 
     show(one);
-    show(two);
 
     return 0;
 }
